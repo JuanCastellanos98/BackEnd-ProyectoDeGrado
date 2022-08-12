8da2cdab11ed7546663c24ee134f98f737c87f2a
JuanCastellanos98 <Juan.angel.castellanos.98@gmail.com>
2022-08-12 11:37:37 -0500

    Cambio de la funcion de simulacion de get a post
    se hizo el cambio debido  a que con el get se producia un error al llamarlo con el front de la aplicacion

--- HTA-DM/src/main/java/com/uis/simom/htadm/rest/SimulacionREST.java
+++ HTA-DM/src/main/java/com/uis/simom/htadm/rest/SimulacionREST.java
@@ -5,16 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.uis.simom.htadm.service.SimulacionService;
 import com.uis.simom.htadm.model.Simulacion;
 //import com.uis.simom.htadm.model.SimulacionPaciente;
 import com.uis.simom.htadm.model.ModeloSimulacion;
-
 
 @RestController
 @RequestMapping("/simulaciones/")
@@ -28,19 +26,17 @@ public class SimulacionREST {
 		return ResponseEntity.ok(simulacionService.findAll());
 	}
 
-	@GetMapping("frami")
+	@PostMapping
 	private ModeloSimulacion simuFra(@RequestBody Simulacion simulacionpaciente) {
-		
 		ModeloSimulacion modeloSimulacion = new ModeloSimulacion();
-		//System.out.println(simulacionpaciente);
 		modeloSimulacion.calcularRiesgo(simulacionpaciente.getEdad(), simulacionpaciente.getTiempo(),
 				simulacionpaciente.getSexo(), simulacionpaciente.getAltura(), simulacionpaciente.getHerencia(),
 				simulacionpaciente.getFumar(), simulacionpaciente.getCalorias(), simulacionpaciente.getUpDownCalorias(),
 				simulacionpaciente.getUpDown(), simulacionpaciente.getPeso(), simulacionpaciente.getSbp(),
-				simulacionpaciente.getDbp(), simulacionpaciente.getSemanaC(), simulacionpaciente.getSemanaF(), 0, 10, 0, 30, 0, 20, 10, 20, 1, 1, 1, 1, 0, 1);
-		
+				simulacionpaciente.getDbp(), simulacionpaciente.getSemanaC(), simulacionpaciente.getSemanaF(), 0, 10, 0,
+				30, 0, 20, 10, 20, 1, 1, 1, 1, 0, 1);
 		return modeloSimulacion;
-		
+
 	}
 
 }
